71cf1ca4a2f954b219ec6ecbe5b0ebdf799b2357
Luciano Diamand <lucianodiamand@gmail.com>
2022-08-24 10:37:20 -0300

    Add coordinacion to custom provider

--- shell/custom.sh
+++ shell/custom.sh
@@ -8,14 +8,15 @@ OE_CUSTOM_ADDONS="/opt/odoo15/odoo-custom-addons"
 OE_USER="odoo15"
 
 ln -s /vagrant/addons/employee_extended /opt/odoo15/odoo-custom-addons/employee_extended
+ln -s /vagrant/addons/coordinacion /opt/odoo15/odoo-custom-addons/coordinacion
 
 echo -e "* Adding custom addons"
-sed '/^addons_path/ s/$/,\/opt\/odoo15\/odoo-custom-addons\/employee_extended/' /etc/odoo15.conf > /tmp/odoo15.conf
+sed '/^addons_path/ s/$/,\/opt\/odoo15\/odoo-custom-addons\/employee_extended,\/opt\/odoo15\/odoo-custom-addons\/coordinacion/' /etc/odoo15.conf > /tmp/odoo15.conf
 mv /tmp/odoo15.conf /etc/odoo15.conf
 
 systemctl stop odoo15
 
-su $OE_USER -c "/opt/odoo15/odoo/odoo-bin -c /etc/odoo15.conf -d hcd -i employee_extended --without-demo=all --load-language es_AR --stop-after-init" 2> /dev/null
+su $OE_USER -c "/opt/odoo15/odoo/odoo-bin -c /etc/odoo15.conf -d hcd -i employee_extended,coordinacion --without-demo=all --load-language es_AR --stop-after-init" 2> /dev/null
 
 systemctl start odoo15
 
